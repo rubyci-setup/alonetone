ef2d83d23b9d04790fbe637f62e9ef27fa0f0a05
rubyci-setup <rubyci.setup@gmail.com>
2024-12-11 14:56:15 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
+  if ENV["FAST_CI_SECRET_KEY"]
+    require "fast_ci"
+    require "rspec/core/runner"
+    require "fast_ci/runner_prepend"
+
+    class RSpec::Core::ExampleGroup
+      def self.filtered_examples
+        ids = Thread.current[:fastci_scoped_ids] || ""
+
+        RSpec.world.filtered_examples[self].filter do |ex|
+          ids == "" || /^#{ids}($|:)/.match?(ex.metadata[:scoped_id])
+        end
+      end
+    end
+
+    RSpec::Core::Runner.prepend(FastCI::RunnerPrepend)
+  end
 
 require 'webmock/rspec'
 
